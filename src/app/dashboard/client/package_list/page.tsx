ff184efbbfe649221acb19f1fb637a31bd17676c
Vistheu <49154926+vistochicho@users.noreply.github.com>
2024-11-10 18:45:24 +0700

    trying to design package list

---
+++ src/app/dashboard/client/package_list/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Package_List = () => {
+  return (
+    <div className="rounded-lg bg-white">
+      <div className="p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-6 gap-x-6">
+          <div className="sm:col-span-2 border-2 border-black rounded-md p-4 text-center">
+            <h1>Test</h1>
+          </div>
+          <div className="sm:col-span-2">
+            <h1>Test</h1>
+          </div>
+          <div className="sm:col-span-2">
+            <h1>Test</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Package_List;
